44bda91cae518a1d9f2e16a2666f5b10b01d2993
xenon <demiladeafuape74@gmail.com>
2026-03-21 23:48:03 +0100

    updated nexxt config

--- next.config.js
+++ next.config.js
@@ -3,15 +3,17 @@ const nextConfig = {
   images: {
     domains: ['pub-*.r2.dev'],
   },
- async rewrites() {
-  return [
-    {
-      source: '/django-api/:path*',
-      destination: 'https://school-ecosystem-demo.onrender.com/:path*',
-    },
-  ]
-},
-trailingSlash: false,
+  async rewrites() {
+    return {
+      beforeFiles: [
+        {
+          source: '/django-api/:path*',
+          destination: 'https://school-ecosystem-demo.onrender.com/:path*',
+        },
+      ]
+    }
+  },
+  trailingSlash: false,
 }
 
 module.exports = nextConfig
