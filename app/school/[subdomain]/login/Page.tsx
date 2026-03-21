9863dd472ed937f8a2bfa70da412f7a4e23627d5
xenon <demiladeafuape74@gmail.com>
2026-03-21 02:05:14 +0100

    fixed import err

--- app/school/[subdomain]/login/Page.tsx
+++ app/school/[subdomain]/login/Page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { useParams, useRouter } from 'next/navigation'
 import { loginUser, getUser } from '@/lib/ecosystem-api'
 
