ff5b196685b214b5b5d0921cf621b9d0bdc3eb90
xenon <demiladeafuape74@gmail.com>
2026-03-21 02:26:59 +0100

    updated nect config

--- next.config.js
+++ next.config.js
@@ -3,6 +3,14 @@ const nextConfig = {
   images: {
     domains: ['pub-*.r2.dev'],
   },
+  async rewrites() {
+    return [
+      {
+        source: '/django-api/:path*',
+        destination: 'https://school-ecosystem-demo.onrender.com/:path*',
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
